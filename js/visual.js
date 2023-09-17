8f21a3596cfd9d8f96d3f618fd5f584fd1fbbaa3
HamB20001013 <133739490+HamB20001013@users.noreply.github.com>
2023-09-17 16:07:15 +0900

    feat : header 반응형 작업 완료
    header 반응형 작업 완료

--- js/visual.js
+++ js/visual.js
@@ -1,19 +1,30 @@
-window.addEventListener("load", function() {
-  new Swiper('.visual .swiper', {    
-    autoplay: {
-      delay: 4000,
-    },
-    loop: true,
-    slidesPerView: 1,
-    autoHeight: true,
-    pagination: {
-      el: '.promotion .swiper-pagination',
-      clickable: true,
-    },
-    
-    navigation: {
-      prevEl: '.visual .fa-chevron-left',
-      nextEl: '.visual .fa-chevron-right',
-    },
+$( document ).ready( function() {
+	
+  $('.visual .autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
+
+} );
+
+window.addEventListener("load", function() {
+  // new Swiper('.visual .swiper', {    
+  //   autoplay: {
+  //     delay: 4000,
+  //   },
+  //   loop: true,
+  //   slidesPerView: 1,
+  //   autoHeight: true,
+  //   pagination: {
+  //     el: '.promotion .swiper-pagination',
+  //     clickable: true,
+  //   },
+    
+  //   navigation: {
+  //     prevEl: '.visual .fa-chevron-left',
+  //     nextEl: '.visual .fa-chevron-right',
+  //   },
+  // });
 });
