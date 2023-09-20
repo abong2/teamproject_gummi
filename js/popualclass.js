4bc01ec388f8d8448f0f014ffa1bd5a8d9f41fd0
kimhyeseung1 <hg724932@gmail.com>
2023-09-20 19:38:07 +0900

    feat: class 작업중

--- js/popualclass.js
+++ js/popualclass.js
@@ -36,5 +36,50 @@ $(document).ready(function() {
     }
   });
   
+const slidewrap = document.querySelector(".slidewrap");
+const slidescnt = document.querySelectorAll(".slide").length;
+const slideContainer = document.getElementsByClassName("slides");
+const slideWidth = slidewrap.offsetWidth;
+let currentSlide = 0;
+
+//슬라이드 이동 함수
+function goToSlide(index){
+    currentSlide = index;
+    slideContainer[0].style.transition = 'transform 0.5s ease';
+    slideContainer[0].style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+}
+
+//CSS 삽입 함수
+function AddStyle(style){
+    const styleTag = document.createElement('style');
+    styleTag.innerHTML = style;
+    document.head.appendChild(styleTag);
+}
+
+let Autoslideactive = null ; // 슬라이드 자동 이동 호출을 변수 화
+function autoslide(){
+    const index = (currentSlide+1) < slidescnt ? currentSlide+1 : 0;
+    goToSlide(index);
+}
+
+/*  마우스 감지 시 슬라이드 자동 멈추기
+
+*/
+function Createmousedetector(){
+    //마우스가 슬라이드에 올라왔을 때
+    slideContainer[0].addEventListener("mouseover",(event)=>{
+
+        clearInterval(Autoslideactive);
+    })
+    //마우스가 슬라이드에서 나온 경우 다시 실행
+    slideContainer[0].addEventListener("mouseout",(event)=>{
+
+        Autoslideactive = setInterval(autoslide,3000);
+    })
+}
+Autoslideactive = setInterval(autoslide,3000); //일정 시간마다 호출
+Createmousedetector();
+  
 });
+
 
