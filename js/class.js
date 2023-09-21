be4a5443550203e2052f119d27cf7196b0a8b445
kimhyeseung1 <hg724932@gmail.com>
2023-09-21 13:12:01 +0900

    feat: sub_notice 작업중

--- js/class.js
+++ js/class.js
@@ -37,4 +37,19 @@ $(function() {
       $(this).closest('.qna-item').removeClass('on').find('.answer-article').slideUp(300)
     }
   })
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
 });
--- js/sub_notice.js
+++ js/sub_notice.js
@@ -1,4 +1,23 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function() {  
+  // top 버튼 스크롤 기능
+  const topBtn = document.getElementById("top-btn");
+  topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    topBtn.style.transform = "rotate(0deg)"
+    console.log(window.scrollY);
+    if (window.scrollY == 0) {
+      window.scrollTo({
+        top: 99999,
+        behavior: "smooth",
+      });
+      topBtn.style.transform = "rotate(180deg)"
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
   $('.check.all').on('change', function(){
     $('.check').prop('checked', $(this).prop('checked'));
   });
