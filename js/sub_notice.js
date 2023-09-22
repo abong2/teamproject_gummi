e615eeca98dcf8c16334a6fc9a5913646244d091
HamB20001013 <khb1535@gmail.com>
2023-09-22 14:45:34 +0900

    feat : 고정 메뉴 모든 페이지에 적용
    고정 메뉴 모든 페이지에 적용

--- js/sub_notice.js
+++ js/sub_notice.js
@@ -1,23 +1,4 @@
 window.addEventListener("load", function() {  
-  // top 버튼 스크롤 기능
-  const topBtn = document.getElementById("top-btn");
-  topBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    topBtn.style.transform = "rotate(0deg)"
-    console.log(window.scrollY);
-    if (window.scrollY == 0) {
-      window.scrollTo({
-        top: 99999,
-        behavior: "smooth",
-      });
-      topBtn.style.transform = "rotate(180deg)"
-    } else {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-  });
   $('.check.all').on('change', function(){
     $('.check').prop('checked', $(this).prop('checked'));
   });
