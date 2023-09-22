f13585a07490a2fc1f0efc48621d08e9671f23b0
abong <rlatmfrl232@gmail.com>
2023-09-22 17:17:29 +0900

    feat : category 아이콘변경 및 로그인 페이지 작업

--- js/sub_login.js
+++ js/sub_login.js
@@ -1,11 +1,48 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('login_container');
+  //tab menu
+  // var $tab_list = $(".tab_list li"),
+  //     $tab_cont = $(".tab_cont");
+  // var idx = 0;
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+  // $tab_cont.eq(0).show();
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+  // $tab_list.click(function(){
+  //     idx = $(this).index();
+  //     $tab_list.removeClass("active").eq(idx).addClass("active")
+  //     $tab_cont.fadeIn(500)
+  //     if($tab_list.eq(0).hasClass("active")){
+  //         $tab_cont.removeClass("account2")
+  //         $tab_cont.addClass("account1")
+  //     }else{
+  //         $tab_cont.removeClass("account1")
+  //         $tab_cont.addClass("account2")
+  //     }
+  // })
+
+  //tab menu
+  window.addEventListener("load", function() {
+  var tabListItems = document.querySelectorAll(".tab_list li");
+  var tabContents = document.querySelectorAll(".tab_cont");
+  var idx = 0;
+
+  tabListItems.forEach(function (item, index) {
+    item.addEventListener("click", function () {
+      idx = index;
+      tabListItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      tabListItems[idx].classList.add("active");
+
+      if (tabListItems[0].classList.contains("active")) {
+        tabContents.forEach(function (item) {
+          item.classList.remove("account2");
+          item.classList.add("account1");
+        });
+      } else {
+        tabContents.forEach(function (item) {
+          item.classList.remove("account1");
+          item.classList.add("account2");
+        });
+      }
+    });
+  });
+})
